e9df98955d32305659e5177fd2559831ae7da30a
cjenewein <caroline_jenewein@hotmail.com>
2018-09-14 13:46:00 -0500

    Maverick Food Service new web application!

---
+++ crm/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+from .models import Customer, Service, Product, Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+				list_display = ['user',	'date_of_birth',	'photo']
+
+class CustomerList(admin.ModelAdmin):
+    list_display = ( 'cust_name', 'organization', 'phone_number' )
+    list_filter = ( 'cust_name', 'organization')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+
+class ServiceList(admin.ModelAdmin):
+    list_display = ( 'cust_name', 'service_category', 'setup_time')
+    list_filter = ( 'cust_name', 'setup_time')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+class ProductList(admin.ModelAdmin):
+    list_display = ( 'cust_name', 'product', 'pickup_time')
+    list_filter = ( 'cust_name', 'pickup_time')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+
+admin.site.register(Customer, CustomerList)
+admin.site.register(Service, ServiceList)
+admin.site.register(Product, ProductList)
+
+
---
+++ crm/urls.py
@@ -0,0 +1,40 @@
+from django.conf.urls import url
+from django.contrib.auth import	views as auth_views
+from . import views
+from django.urls import path, reverse_lazy
+
+
+app_name = 'crm'
+urlpatterns = [
+    path('', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # customer
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/create/', views.customer_new, name='customer_new'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('customer/<int:pk>/summary/', views.summary, name='summary'),
+    # service
+    path('service_list', views.service_list, name='service_list'),
+    path('service/create/', views.service_new, name='service_new'),
+    path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
+    path('service/<int:pk>/delete/', views.service_delete, name='service_delete'),
+    # product
+    path('product_list', views.product_list, name='product_list'),
+    path('product/create/', views.product_new, name='product_new'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+]
+
